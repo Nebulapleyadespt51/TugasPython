4b2ad0df58a949862a0a8db7451c2894de859030
Nebulapleyadespt51 <113223373+Nebulapleyadespt51@users.noreply.github.com>
2022-09-10 10:26:50 +0700

    Pelatihan/rentetan python LAB TI

---
+++ Nebula.py
@@ -0,0 +1,59 @@
+def fungsi_python():
+   print("hai ini saya!")
+
+
+nama = "Nebula Pleyades Putra Taurus"
+umur = 900
+ukuransepatu = 40.5
+print("kamu siapa? " + nama + " umur kamu berapa abad? " + str(umur))
+print("Umur kamu berapa abad? : ", umur)
+print(ukuransepatu, "ukuranku adalah", type(ukuransepatu))
+
+
+# boolean
+masukkelas = True
+pulangkelas = False
+
+if(masukkelas == False):
+    print("belum masuk lah cuy")
+
+print(masukkelas)
+
+# list <array>
+
+hewan = ["kelinci", "dinosaurus", "ayam"]
+hewanlaut = ("ikan lele", "ikan paus")
+namakawan = {"abdul", "wicaksono", "irgi", "zaki"}
+print(hewan[0])
+print(hewanlaut[1])
+print(namakawan)
+
+x = 10
+y = 7
+z = 8
+print("penjumlahan " + str(x + y + z))
+
+x = 20
+y = 10
+z = 5
+print("pengurangan :" + str(x - y - z))
+
+x = 12
+y = 6
+print("pembagian : " + str(x // y))
+
+x = 12
+y = 4
+print("perkalian : " + str(x*y))
+
+x = 10
+y = 8
+print("modulus : " + str(x % y))
+
+# fungsi_python()
+# INI KOMENTAR
+
+print()
+
+name = input("Masukkan akun anda : ")
+print("")
